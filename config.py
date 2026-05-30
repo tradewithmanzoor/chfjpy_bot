d242b577963670c05c904b2fcfb63b9bc328198d
tradewithmanzoor <manzoorpathan899@gmail.com>
2026-05-31 01:31:49 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ import os
 
 load_dotenv()  # load from .env
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-TWELVE_DATA_API_KEY = os.getenv("TWELVE_DATA_API_KEY")
+TELEGRAM_BOT_TOKEN = 8815982502:AAGx1VChZVm6iEzRkXe_Dwa__chTDSvdGqw
+TWELVE_DATA_API_KEY = b7f561f3507a437d95cc002a39fd6333
